474496d69743d9c89c2bcff05d7b6fc95dea5517
Hystrix <2669731677@qq.com>
2024-05-28 22:48:51 +0800

    first

---
+++ heelo/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.hystrix.exceldemo.exception;
+
+
+import com.hystrix.exceldemo.common.BaseResponse;
+import com.hystrix.exceldemo.common.ErrorCode;
+import com.hystrix.exceldemo.common.ResultUtils;
+import com.hystrix.exceldemo.exception.ex.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
+
+/**
+ * 全局异常处理器
+ *
+ * @author yupi
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    //继续抛出给AliAccessDeniedHandler处理
+    @ExceptionHandler(AccessDeniedException.class)
+    public void defaultExceptionHandler(AccessDeniedException e) throws AccessDeniedException{
+        throw e;
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage());
+    }
+}
